15d605904df3ef71d4ebc04ad57fb97554a407d0
JommyV <tavaresjmv@gmail.com>
2025-05-19 11:51:58 +0100

    Version 0.1.1.0. Hooked Menus, and added c++ code for widget (WIP).

--- Source/BotBlastFinal/Private/SatchelsUIBase.cpp
+++ Source/BotBlastFinal/Private/SatchelsUIBase.cpp
@@ -2,3 +2,154 @@
 
 
 #include "BotBlastFinal/Public/SatchelsUIBase.h"
+#include "Components/Border.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "Components/VerticalBox.h"
+#include "Components/VerticalBoxSlot.h"
+#include "Blueprint/UserWidget.h"
+
+
+/*void USatchelsUIBase::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	UE_LOG(LogTemp, Warning, TEXT("NativeOnInitialized()"));
+	UpdateWidget();
+}*/
+
+void USatchelsUIBase::OnIntStatUpdated(int32 OldValue, int32 NewValue, int32 MaxValue)
+{
+	// Just use the float version of the function!
+	OnFloatStatUpdated(static_cast<float>(OldValue), static_cast<float>(NewValue), static_cast<float>(MaxValue));
+}
+
+void USatchelsUIBase::OnFloatStatUpdated(float OldValue, float NewValue, float MaxValue)
+{
+	// For now, we can ignore the OldValue, this will be used later for animating the bar.
+
+	// Calculate the new percentage, and clamp the value between 0 and 1,
+	// just in case someone passes invalid values
+
+	// Prevent divide by zero errors.
+	// The macro SMALL_NUMBER would be appropriate here..
+	// but KINDA_SMALL_NUMBER is the sort of Name which goes against all the 'stuffy' coding conventions,
+	// and it's official EPIC code, so I wanted to highlight that! ;-)
+	// I think its 'KINDA' amusing.
+	if (MaxValue == 0.f) MaxValue = KINDA_SMALL_NUMBER;
+
+	CurrentPercentage = FMath::Clamp(NewValue / MaxValue, 0.f, 1.f);
+	CurrentValue      = NewValue;
+	UpdateWidget();
+}
+
+void USatchelsUIBase::ProcessCurrentValueText()
+{
+	// Ultimately this should be handled in a culture appropriate matter,
+	// i.e suffixes like 'k' for thousands should be dependant on culture,
+	// and may be prefixes in some cultures.
+	// HOWEVER - for the purposes of this tutorial, we will keep it simple
+
+	// if the number is <10 then display it as a float to 2DP : 0.01
+	// if the number is <100, then display it as a float with 1DP : 99.9
+	// if the number is <1000 then display it as an integer with 0DP: 986
+	// if the number is >= 1000, then divide it by 1000 and apply the rules above, and add a 'k' on the end
+
+	FString FloatString;
+
+	if (CurrentValue < 1000.f)
+	{
+		FloatString = FString::SanitizeFloat(CurrentValue);
+
+		if (CurrentValue < 100.f)
+		{
+			int32 StringLen = FloatString.Len();
+			if (StringLen > 4)
+				FloatString = FloatString.Left(4);
+			else if (StringLen < 4)
+				FloatString = FloatString.Append("0", 4 - StringLen);
+		}
+		else
+		{
+			FloatString = FloatString.Left(3);
+		}
+	}
+	else
+	{
+		// scaled value
+		float ScaledValue = CurrentValue / 1000.f;
+		FloatString       = FString::SanitizeFloat(ScaledValue);
+		if (ScaledValue < 10.f)
+			FloatString = FloatString.Left(3).Append(TEXT("k"));
+		else
+		{
+			FloatString = FloatString.Left(2).Append(TEXT("k"));
+		}
+	}
+
+	CurrentValueText = FText::FromString(FloatString);
+}
+
+void USatchelsUIBase::UpdateWidget()
+{
+	// Check that the controls we want actually exist
+	if (!Bar_Filled ||
+		!Bar_Empty ||
+		!MainBorder ||
+		!Icon) return;
+
+	FSlateChildSize EmptySize = FSlateChildSize(ESlateSizeRule::Fill);
+	EmptySize.Value           = 1.f - CurrentPercentage;
+
+	FSlateChildSize FilledSize = FSlateChildSize(ESlateSizeRule::Fill);
+	FilledSize.Value           = CurrentPercentage;
+
+	if (UVerticalBoxSlot* FilledSlot = Cast<UVerticalBoxSlot>(Bar_Filled->Slot))
+		FilledSlot->SetSize(FilledSize);
+
+	if (UVerticalBoxSlot* EmptySlot = Cast<UVerticalBoxSlot>(Bar_Empty->Slot))
+		EmptySlot->SetSize(EmptySize);
+
+	MainBorder->SetBrushColor(BarBackgroundColor);
+	Bar_Filled->SetBrushColor(BarForegroundColor);
+	Icon->SetBrush(IconBrush);
+
+	ProcessCurrentValueText();
+
+	ValueText->SetText(CurrentValueText);
+
+	PercentBars->SetVisibility(IsFullSize ? ESlateVisibility::Visible : ESlateVisibility::Collapsed);
+}
+
+#if WITH_EDITOR
+
+void USatchelsUIBase::OnDesignerChanged(const FDesignerChangedEventArgs& EventArgs)
+{
+	Super::OnDesignerChanged(EventArgs);
+	// Update the widget, after editor changes due to layout,
+	// for example, resizing the widget, or a container that the widget is in.
+	UpdateWidget();
+}
+
+void USatchelsUIBase::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	// Update the widget, after it's properties have been changed.
+
+	// In this case it IS sufficient to just call UpdateWidget()
+	// which means changing ANY property will trigger the update.
+	// The commented code demonstrates one way, you could run specific code
+	// when individual properties are changed, perhaps because you want to
+	// do specific things only when specific properties are changed in the editor.
+
+	//const FString PropertyName = ((PropertyChangedEvent.Property != nullptr)
+	//	                              ? PropertyChangedEvent.Property->GetFName()
+	//	                              : NAME_None).ToString();
+
+	//if (PropertyName == TEXT("CurrentPercentage") || PropertyName == TEXT("CurrentValue"))
+	//{
+		UpdateWidget();
+	//}
+}
+
+#endif
--- Source/BotBlastFinal/Public/SatchelsUIBase.h
+++ Source/BotBlastFinal/Public/SatchelsUIBase.h
@@ -4,13 +4,93 @@
 
 #include "CoreMinimal.h"
 #include "BotBlastFinal/WidgetBase.h"
+#include "Blueprint/UserWidget.h"
 #include "SatchelsUIBase.generated.h"
 
 /**
  * 
  */
+
+class UVerticalBox;
+class UBorder;
+class UImage;
+class UTextBlock;
+
+
 UCLASS()
 class BOTBLASTFINAL_API USatchelsUIBase : public UWidgetBase
 {
+public:
+	// Function that can be called to update the bar using int values
+	UFUNCTION()
+	void OnIntStatUpdated(int32 OldValue, int32 NewValue, int32 MaxValue);
+
+	// Function that can be called to update the bar using float values
+	UFUNCTION()
+	void OnFloatStatUpdated(float OldValue, float NewValue, float MaxValue);
+
+#if WITH_EDITOR
+	virtual void OnDesignerChanged(const FDesignerChangedEventArgs& EventArgs) override;
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "Constituent Controls", meta = (BindWidget))
+	TObjectPtr<UBorder> MainBorder = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Constituent Controls", meta = (BindWidget))
+	TObjectPtr<UVerticalBox> PercentBars = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Constituent Controls", meta = (BindWidget))
+	TObjectPtr<UImage> Icon = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Constituent Controls", meta = (BindWidget))
+	TObjectPtr<UBorder> Bar_Empty = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Constituent Controls", meta = (BindWidget))
+	TObjectPtr<UBorder> Bar_Filled = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Constituent Controls", meta = (BindWidget))
+	TObjectPtr<UTextBlock> ValueText = nullptr;
+
+
+private:
+	UPROPERTY(EditAnywhere, Category="Stat Bar")
+	FSlateBrush IconBrush;
+
+	UPROPERTY(EditAnywhere, Category="Stat Bar")
+	FLinearColor BarBackgroundColor = FLinearColor(0.3f, 0.f, 0.f, 0.3f);
+
+	UPROPERTY(EditAnywhere, Category="Stat Bar")
+	FLinearColor BarForegroundColor = FLinearColor(1.f, 0.f, 0.f, 0.75f);
+
+	// Display the Bar as full size, or minimized
+	UPROPERTY(EditAnywhere, Category="Stat Bar")
+	bool IsFullSize = true;
+
+	// Internal variable to store the current 'filled' amount
+	// 'Clamped' to stop the value going outside of what we consider a % to be
+	UPROPERTY(EditAnywhere, Category="Stat Bar|Testing",
+		meta=(ClampMin=0, UIMin=0, ClampMax=1, UIMax=1, Units="Percent"))
+	float CurrentPercentage = 0.f;
+
+	// This is the 'actual' value of the current amount
+	UPROPERTY(EditAnywhere, Category="Stat Bar|Testing", meta=(ClampMin=0, UIMin=0))
+	float CurrentValue = 100.f;
+
+	// This does not need to be a property as it is never serialized,
+	// and is totally dependent on the CurrentValue.
+	FText CurrentValueText;
+
+	// This function works out what should be displayed in the TextBox,
+	// based on the CurrentValue.
+	void ProcessCurrentValueText();
+
+	// Called after any changes are made to redraw the bar
+	void UpdateWidget();
+
+
 	GENERATED_BODY()
+	
 };

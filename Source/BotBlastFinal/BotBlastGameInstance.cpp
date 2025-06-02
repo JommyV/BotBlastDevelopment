d9c87c8666ecccbbe2518299ce528e7d93f5f35e
JommyV <tavaresjmv@gmail.com>
2025-06-02 03:02:15 +0100

    Fixed build default settings.

---
+++ Source/BotBlastFinal/BotBlastGameInstance.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BotBlastGameInstance.h"
+#include "GameFramework/GameUserSettings.h"
+#include "Kismet/GameplayStatics.h"
+
+void UBotBlastGameInstance::Init()
+{
+	Super::Init();
+
+	// Force graphics preset to "High"
+	UGameUserSettings* Settings = GEngine->GetGameUserSettings();
+	if (Settings)
+	{
+		Settings->SetOverallScalabilityLevel(1); // 0=Low, 1=Med, 2=High, 3=Epic
+		Settings->ApplySettings(false);          // false = no restart needed
+		Settings->SaveSettings();
+	}
+}
---
+++ Source/BotBlastFinal/BotBlastGameInstance.h
@@ -0,0 +1,16 @@
+﻿
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "BotBlastGameInstance.generated.h"
+
+UCLASS()
+class BOTBLASTFINAL_API UBotBlastGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Init() override;
+};
